197abd94e306ed26688659369454531d7344f45a
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-01-06 14:27:09 -0800

    added milliseconds toggle

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,20 +31,23 @@ namespace Stopwatch
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-			this.lblHours = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.lblMinutes = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.lblSeconds = new System.Windows.Forms.Label();
-			this.start = new System.Windows.Forms.PictureBox();
-			this.resume = new System.Windows.Forms.PictureBox();
-			this.stop = new System.Windows.Forms.PictureBox();
 			this.reset = new System.Windows.Forms.PictureBox();
+			this.stop = new System.Windows.Forms.PictureBox();
+			this.resume = new System.Windows.Forms.PictureBox();
+			this.start = new System.Windows.Forms.PictureBox();
+			this.lblHours = new System.Windows.Forms.Label();
+			this.colon1 = new System.Windows.Forms.Label();
+			this.lblMinutes = new System.Windows.Forms.Label();
+			this.colon2 = new System.Windows.Forms.Label();
+			this.lblSeconds = new System.Windows.Forms.Label();
+			this.colon3 = new System.Windows.Forms.Label();
+			this.lblMilliseconds = new System.Windows.Forms.Label();
+			this.toggleMilliseconds = new System.Windows.Forms.Label();
 			this.tableLayoutPanel2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.resume)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.stop)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.reset)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.stop)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.resume)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel2
@@ -64,99 +67,9 @@ namespace Stopwatch
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
 			this.tableLayoutPanel2.Size = new System.Drawing.Size(515, 35);
 			this.tableLayoutPanel2.TabIndex = 1;
-			// 
-			// lblHours
-			// 
-			this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-			this.lblHours.Location = new System.Drawing.Point(85, 9);
-			this.lblHours.Name = "lblHours";
-			this.lblHours.Size = new System.Drawing.Size(69, 49);
-			this.lblHours.TabIndex = 2;
-			this.lblHours.Text = "00";
-			this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// label2
-			// 
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-			this.label2.Location = new System.Drawing.Point(160, 9);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(69, 49);
-			this.label2.TabIndex = 3;
-			this.label2.Text = ":";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// lblMinutes
-			// 
-			this.lblMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-			this.lblMinutes.Location = new System.Drawing.Point(235, 9);
-			this.lblMinutes.Name = "lblMinutes";
-			this.lblMinutes.Size = new System.Drawing.Size(69, 49);
-			this.lblMinutes.TabIndex = 4;
-			this.lblMinutes.Text = "00";
-			this.lblMinutes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// label4
-			// 
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-			this.label4.Location = new System.Drawing.Point(310, 9);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(69, 49);
-			this.label4.TabIndex = 5;
-			this.label4.Text = ":";
-			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// lblSeconds
-			// 
-			this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-			this.lblSeconds.Location = new System.Drawing.Point(385, 9);
-			this.lblSeconds.Name = "lblSeconds";
-			this.lblSeconds.Size = new System.Drawing.Size(69, 49);
-			this.lblSeconds.TabIndex = 6;
-			this.lblSeconds.Text = "00";
-			this.lblSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// start
-			// 
-			this.start.BackColor = System.Drawing.SystemColors.ControlLight;
-			this.start.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
-			this.start.Location = new System.Drawing.Point(3, 3);
-			this.start.Name = "start";
-			this.start.Size = new System.Drawing.Size(122, 29);
-			this.start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.start.TabIndex = 0;
-			this.start.TabStop = false;
-			this.start.Click += new System.EventHandler(this.start_Click);
-			// 
-			// resume
-			// 
-			this.resume.BackColor = System.Drawing.SystemColors.Control;
-			this.resume.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.resume.Enabled = false;
-			this.resume.Image = ((System.Drawing.Image)(resources.GetObject("resume.Image")));
-			this.resume.Location = new System.Drawing.Point(131, 3);
-			this.resume.Name = "resume";
-			this.resume.Size = new System.Drawing.Size(122, 29);
-			this.resume.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.resume.TabIndex = 1;
-			this.resume.TabStop = false;
-			this.resume.Click += new System.EventHandler(this.resume_Click);
-			// 
-			// stop
-			// 
-			this.stop.BackColor = System.Drawing.SystemColors.Control;
-			this.stop.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.stop.Enabled = false;
-			this.stop.Image = ((System.Drawing.Image)(resources.GetObject("stop.Image")));
-			this.stop.Location = new System.Drawing.Point(259, 3);
-			this.stop.Name = "stop";
-			this.stop.Size = new System.Drawing.Size(122, 29);
-			this.stop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.stop.TabIndex = 2;
-			this.stop.TabStop = false;
-			this.stop.Click += new System.EventHandler(this.stop_Click);
 			// 
 			// reset
 			// 
@@ -172,17 +85,145 @@ namespace Stopwatch
 			this.reset.TabStop = false;
 			this.reset.Click += new System.EventHandler(this.reset_Click);
 			// 
+			// stop
+			// 
+			this.stop.BackColor = System.Drawing.SystemColors.Control;
+			this.stop.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.stop.Enabled = false;
+			this.stop.Image = ((System.Drawing.Image)(resources.GetObject("stop.Image")));
+			this.stop.Location = new System.Drawing.Point(259, 3);
+			this.stop.Name = "stop";
+			this.stop.Size = new System.Drawing.Size(122, 29);
+			this.stop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.stop.TabIndex = 2;
+			this.stop.TabStop = false;
+			this.stop.Click += new System.EventHandler(this.stop_Click);
+			// 
+			// resume
+			// 
+			this.resume.BackColor = System.Drawing.SystemColors.Control;
+			this.resume.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.resume.Enabled = false;
+			this.resume.Image = ((System.Drawing.Image)(resources.GetObject("resume.Image")));
+			this.resume.Location = new System.Drawing.Point(131, 3);
+			this.resume.Name = "resume";
+			this.resume.Size = new System.Drawing.Size(122, 29);
+			this.resume.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.resume.TabIndex = 1;
+			this.resume.TabStop = false;
+			this.resume.Click += new System.EventHandler(this.resume_Click);
+			// 
+			// start
+			// 
+			this.start.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.start.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
+			this.start.Location = new System.Drawing.Point(3, 3);
+			this.start.Name = "start";
+			this.start.Size = new System.Drawing.Size(122, 29);
+			this.start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.start.TabIndex = 0;
+			this.start.TabStop = false;
+			this.start.Click += new System.EventHandler(this.start_Click);
+			// 
+			// lblHours
+			// 
+			this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblHours.Location = new System.Drawing.Point(85, 9);
+			this.lblHours.Name = "lblHours";
+			this.lblHours.Size = new System.Drawing.Size(69, 49);
+			this.lblHours.TabIndex = 2;
+			this.lblHours.Text = "00";
+			this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// colon1
+			// 
+			this.colon1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.colon1.Location = new System.Drawing.Point(160, 9);
+			this.colon1.Name = "colon1";
+			this.colon1.Size = new System.Drawing.Size(69, 49);
+			this.colon1.TabIndex = 3;
+			this.colon1.Text = ":";
+			this.colon1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblMinutes
+			// 
+			this.lblMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblMinutes.Location = new System.Drawing.Point(235, 9);
+			this.lblMinutes.Name = "lblMinutes";
+			this.lblMinutes.Size = new System.Drawing.Size(69, 49);
+			this.lblMinutes.TabIndex = 4;
+			this.lblMinutes.Text = "00";
+			this.lblMinutes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// colon2
+			// 
+			this.colon2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.colon2.Location = new System.Drawing.Point(310, 9);
+			this.colon2.Name = "colon2";
+			this.colon2.Size = new System.Drawing.Size(69, 49);
+			this.colon2.TabIndex = 5;
+			this.colon2.Text = ":";
+			this.colon2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblSeconds
+			// 
+			this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblSeconds.Location = new System.Drawing.Point(385, 9);
+			this.lblSeconds.Name = "lblSeconds";
+			this.lblSeconds.Size = new System.Drawing.Size(69, 49);
+			this.lblSeconds.TabIndex = 6;
+			this.lblSeconds.Text = "00";
+			this.lblSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// colon3
+			// 
+			this.colon3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.colon3.ForeColor = System.Drawing.SystemColors.Control;
+			this.colon3.Location = new System.Drawing.Point(386, 9);
+			this.colon3.Name = "colon3";
+			this.colon3.Size = new System.Drawing.Size(69, 49);
+			this.colon3.TabIndex = 7;
+			this.colon3.Text = ".";
+			this.colon3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblMilliseconds
+			// 
+			this.lblMilliseconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblMilliseconds.ForeColor = System.Drawing.SystemColors.Control;
+			this.lblMilliseconds.Location = new System.Drawing.Point(461, 9);
+			this.lblMilliseconds.Name = "lblMilliseconds";
+			this.lblMilliseconds.Size = new System.Drawing.Size(69, 49);
+			this.lblMilliseconds.TabIndex = 8;
+			this.lblMilliseconds.Text = "000";
+			this.lblMilliseconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// toggleMilliseconds
+			// 
+			this.toggleMilliseconds.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.toggleMilliseconds.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.toggleMilliseconds.Location = new System.Drawing.Point(12, 102);
+			this.toggleMilliseconds.Name = "toggleMilliseconds";
+			this.toggleMilliseconds.Size = new System.Drawing.Size(515, 29);
+			this.toggleMilliseconds.TabIndex = 10;
+			this.toggleMilliseconds.Text = "Toggle Milliseconds";
+			this.toggleMilliseconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.toggleMilliseconds.Click += new System.EventHandler(this.toggleMilliseconds_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(539, 107);
+			this.ClientSize = new System.Drawing.Size(539, 142);
+			this.Controls.Add(this.toggleMilliseconds);
 			this.Controls.Add(this.lblSeconds);
-			this.Controls.Add(this.label4);
+			this.Controls.Add(this.colon2);
 			this.Controls.Add(this.lblMinutes);
-			this.Controls.Add(this.label2);
+			this.Controls.Add(this.colon1);
 			this.Controls.Add(this.lblHours);
 			this.Controls.Add(this.tableLayoutPanel2);
+			this.Controls.Add(this.lblMilliseconds);
+			this.Controls.Add(this.colon3);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
@@ -191,10 +232,10 @@ namespace Stopwatch
 			this.ShowIcon = false;
 			this.Text = "Stopwatch";
 			this.tableLayoutPanel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.resume)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.stop)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.reset)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.stop)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.resume)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -202,14 +243,17 @@ namespace Stopwatch
 		#endregion
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
 		private System.Windows.Forms.Label lblHours;
-		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label colon1;
 		private System.Windows.Forms.Label lblMinutes;
-		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label colon2;
 		private System.Windows.Forms.Label lblSeconds;
 		private System.Windows.Forms.PictureBox start;
 		private System.Windows.Forms.PictureBox reset;
 		private System.Windows.Forms.PictureBox stop;
 		private System.Windows.Forms.PictureBox resume;
+		private System.Windows.Forms.Label colon3;
+		private System.Windows.Forms.Label lblMilliseconds;
+		private System.Windows.Forms.Label toggleMilliseconds;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,14 +16,16 @@ namespace Stopwatch
 
 			stopwatch = new stopwatch();
 			updater = new Timer { Interval = 1 };
+			toggleMilliseconds_Click(this, null);
 
 			updater.Tick += (sender, e) =>
 			{
 				hours = stopwatch.Elapsed.Hours;
 				minutes = stopwatch.Elapsed.Minutes;
 				seconds = stopwatch.Elapsed.Seconds;
+				milliseconds = stopwatch.Elapsed.Milliseconds;
 
-				if (hours > 24 && (minutes == 60 && seconds == 60))
+				if (hours > 24 && (minutes == 59 && seconds == 59 && milliseconds == 999 ))
 				{
 					MessageBox.Show("Time limit exceeded. Please reset Stopwatch to continue.", "Time Limit Exceeded", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
@@ -31,6 +33,7 @@ namespace Stopwatch
 				lblHours.Text = string.Format("{0:D2}", hours);
 				lblMinutes.Text = string.Format("{0:D2}", minutes);
 				lblSeconds.Text = string.Format("{0:D2}", seconds);
+				lblMilliseconds.Text = string.Format("{0:D3}", milliseconds);
 
 				Application.DoEvents();
 			};
@@ -43,6 +46,7 @@ namespace Stopwatch
 		private int hours = 0;
 		private int minutes = 0;
 		private int seconds = 0;
+		private int milliseconds = 0;
 
 		private void start_Click(object sender, System.EventArgs e)
 		{
@@ -113,6 +117,52 @@ namespace Stopwatch
 			lblHours.Text = hours.ToString("00");
 			lblMinutes.Text = minutes.ToString("00");
 			lblSeconds.Text = seconds.ToString("00");
+		}
+
+		private bool millisecondsShowing = true;
+
+		private void toggleMilliseconds_Click(object sender, EventArgs e)
+		{
+			if (millisecondsShowing)
+			{
+				Transition toggle1 = new Transition(new TransitionType_Linear(1000));
+				toggle1.add(lblHours, "Left", 85);
+				toggle1.add(colon1, "Left", 160);
+				toggle1.add(lblMinutes, "Left", 235);
+				toggle1.add(colon2, "Left", 310);
+				toggle1.add(lblSeconds, "Left", 385);
+				toggle1.add(lblMilliseconds, "ForeColor", SystemColors.Control);
+
+				Transition toggle2 = new Transition(new TransitionType_Linear(500));
+				toggle2.add(colon3, "ForeColor", SystemColors.Control);
+				
+				toggle2.run();
+				toggle1.run();
+
+				millisecondsShowing = false;
+			}
+			else if (!millisecondsShowing)
+			{
+				Transition toggle3 = new Transition(new TransitionType_Linear(1000));
+				toggle3.add(lblHours, "Left", 11);
+				toggle3.add(colon1, "Left", 86);
+				toggle3.add(lblMinutes, "Left", 161);
+				toggle3.add(colon2, "Left", 236);
+				toggle3.add(lblSeconds, "Left", 311);
+				toggle3.add(lblMilliseconds, "ForeColor", SystemColors.ControlText);
+
+				Transition toggle4 = new Transition(new TransitionType_Linear(500));
+				toggle4.add(colon3, "ForeColor", SystemColors.ControlText);
+
+				toggle3.TransitionCompletedEvent += (sender_, e_) =>
+				{
+					toggle4.run();
+				};
+
+				toggle3.run();
+
+				millisecondsShowing = true;
+			}
 		}
 	}
 }
